406ff22c43e583c1ad8703442a2c009a72e9d4e1
Samuel Oliveira <samueldevbr@gmail.com>
2022-01-20 17:38:45 -0300

    creating juno service provider

---
+++ src/Providers/JunoServiceProvider.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Juno\Providers;
+
+use Juno\Support\Services\Auth;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\ServiceProvider;
+
+class JunoServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/juno.php' => config_path('juno.php')
+        ], 'juno-config');
+
+        $clientId = Config::get('juno.api.client_id');
+        $clientSecret = Config::get('juno.api.client_secret');
+        $privateToken = Config::get('juno.api.private_token');
+
+        $authBaseUrl = Config::get('juno.api.environment') === 'sandbox' ?
+            'https://sandbox.boletobancario.com/authorization-server' :
+            'https://api.juno.com.br/authorization-server';
+
+        $resourceBaseUrl = Config::get('juno.api.environment') === 'sandbox' ?
+            'https://sandbox.boletobancario.com/api-integration' :
+            'https://api.juno.com.br';
+
+        Http::macro(
+            'junoAuth',
+            fn () => Http::withBasicAuth($clientId, $clientSecret)->baseUrl($authBaseUrl)
+        );
+
+        Http::macro(
+            'juno',
+            fn () => function () use ($privateToken, $resourceBaseUrl) {
+                if (!Auth::isAuthorized()) {
+                    Auth::authorize();
+                }
+
+                Http::withToken(Auth::getToken())->withHeaders([
+                    'X-Resource-Token' => $privateToken,
+                    'X-Api-Version' => Config::get('juno.api.version')
+                ])->baseUrl($resourceBaseUrl);
+            }
+        );
+    }
+}
