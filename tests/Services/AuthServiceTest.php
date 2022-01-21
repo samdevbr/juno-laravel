3cdd307cf3601bdc5bfc35e3b2f761c792a4902d
Samuel Oliveira <samueldevbr@gmail.com>
2022-01-21 11:49:20 -0300

    testing if failed requests will log and throw correctly

--- tests/Services/AuthServiceTest.php
+++ tests/Services/AuthServiceTest.php
@@ -2,6 +2,9 @@
 
 namespace Juno\Tests;
 
+use Illuminate\Http\Client\RequestException;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Log;
 use Juno\Support\Services\Auth;
 
 class AuthServiceTest extends TestCase
@@ -20,7 +23,24 @@ class AuthServiceTest extends TestCase
     public function itCanAuthorizeClient()
     {
         $this->assertTrue(Auth::authorize());
+        $this->assertTrue(Auth::isAuthorized());
         $this->assertNotNull(Auth::getToken());
-        $this->assertStringStartsWith('ey', Auth::getToken());
+    }
+
+    /**
+     * @test
+     */
+    public function itWillThrowIfRequestFails()
+    {
+        Config::set('juno.client_id', '<fake client id>');
+        Config::set('juno.logging', true);
+
+        $this->expectException(RequestException::class);
+
+        Log::shouldReceive('error')
+            ->once()
+            ->withSomeOfArgs('request failed');
+
+        Auth::authorize();
     }
 }
