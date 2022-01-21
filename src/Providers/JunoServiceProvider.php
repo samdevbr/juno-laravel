db4e354bd42c67b70f299f18ba286329ef930730
Samuel Oliveira <samueldevbr@gmail.com>
2022-01-21 11:27:35 -0300

    Develop (#4)
    * updating readme
    
    * Refactoring config file and token storage (#3)
    
    * refactoring config file
    
    * using cache features to store token instead of saving it on the user session

--- src/Providers/JunoServiceProvider.php
+++ src/Providers/JunoServiceProvider.php
@@ -22,15 +22,15 @@ class JunoServiceProvider extends ServiceProvider
 
         $this->mergeConfigFrom(__DIR__ . '/../config/juno.php', 'juno');
 
-        $clientId = Config::get('juno.api.client_id');
-        $clientSecret = Config::get('juno.api.client_secret');
-        $privateToken = Config::get('juno.api.private_token');
+        $clientId = Config::get('juno.client_id');
+        $clientSecret = Config::get('juno.client_secret');
+        $privateToken = Config::get('juno.private_token');
 
-        $authBaseUrl = Config::get('juno.api.environment') === 'sandbox' ?
+        $authBaseUrl = Config::get('juno.environment') === 'sandbox' ?
             'https://sandbox.boletobancario.com/authorization-server' :
             'https://api.juno.com.br/authorization-server';
 
-        $resourceBaseUrl = Config::get('juno.api.environment') === 'sandbox' ?
+        $resourceBaseUrl = Config::get('juno.environment') === 'sandbox' ?
             'https://sandbox.boletobancario.com/api-integration' :
             'https://api.juno.com.br';
 
@@ -38,7 +38,7 @@ class JunoServiceProvider extends ServiceProvider
             'junoAuth',
             fn () => Http::withBasicAuth($clientId, $clientSecret)
                 ->withHeaders([
-                    'X-Api-Version' => Config::get('juno.api.version')
+                    'X-Api-Version' => Config::get('juno.version')
                 ])
                 ->baseUrl($authBaseUrl)
         );
@@ -52,7 +52,7 @@ class JunoServiceProvider extends ServiceProvider
 
                 Http::withToken(Auth::getToken())->withHeaders([
                     'X-Resource-Token' => $privateToken,
-                    'X-Api-Version' => Config::get('juno.api.version')
+                    'X-Api-Version' => Config::get('juno.version')
                 ])->baseUrl($resourceBaseUrl);
             }
         );
